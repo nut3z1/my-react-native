07c61e45aa62a761ba56edb3b080d544e3b860d3
ITBU\tuannv595 <tuannv595@viettel.com.vn>
2025-10-15 16:51:19 +0700

    update

--- constants/index.ts
+++ constants/index.ts
@@ -1,4 +1,4 @@
 export const CLOUDINARY_CLOUD_NAME = "dfhlihy8n";
 export const CLOUDINARY_UPLOAD_PRESET = "images";
-export const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/dfhlihy8n/image/upload`;
+export const CLOUDINARY_API_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/image/upload`;
 export const MAX_FILE_SIZE = 5 * 1024 * 1024;
